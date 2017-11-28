6cab0eb539a204660dc04667fe0b73eca87d8f70
Marcin Styczen <test@github.com>
2017-11-28 21:17:30 +0100

    TEST

--- src/com/company/test/TEST.java
+++ src/com/company/test/TEST.java
@@ -1,47 +1,117 @@
-package com.company;
-
-import com.company.interfaces.ImageGeneratorConfigurationInterface;
-import com.company.interfaces.ImageGeneratorInterface;
 import javafx.util.Pair;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class Main {
+interface ImageGeneratorConfigurationInterface {
+    /**
+     * Metoda ustawia plotno, po ktorym nalezy rysowac.
+     *
+     * @param canvas
+     *            plotno obrazu
+     */
+    public void setCanvas(boolean[][] canvas);
 
-    public static void main(String[] args) {
-        ImageGenerator imageGenerator = new ImageGenerator();
-        boolean[][] canvas = {{false, false, false, false, false, false, false, false, false, false, false},
-                {false, false, false, false, false, false, false, false, false, false, false},
-                {false, false, false, false, false, false, false, false, false, false, false},
-                {false, false, false, false, false, false, false, false, false, false, false},
-                {false, false, false, false, false, false, false, false, false, false, false},
-                {false, false, false, false, false, false, false, false, false, false, false},
-                {false, false, false, false, false, false, false, false, false, false, false},
-                {false, false, false, false, false, false, false, false, false, false, false},
-                {false, false, false, false, false, false, false, false, false, false, false},
-                {false, false, false, false, false, false, false, false, false, false, false},
-                {false, false, false, false, false, false, false, false, false, false, false}};
-        imageGenerator.setCanvas(canvas);
-        imageGenerator.setInitialPosition(2, 0);
+    /**
+     * Metoda ustawia poczatkowa pozycje piora. Pioro zostaje ustawione na danej
+     * pozycji i zostawia na niej slad.
+     *
+     * @param col
+     *            kolumna, w ktorej umieszczane jest pioro
+     * @param row
+     *            wiersz, w ktorym umieszczane jest pioro
+     */
+    public void setInitialPosition(int col, int row);
 
-        imageGenerator.up(4);
-        imageGenerator.right(3);
-        imageGenerator.down(2);
-        imageGenerator.repeat(3);
-        imageGenerator.undo(2);
-        imageGenerator.redo(1);
-        imageGenerator.up(3);
-        imageGenerator.repeat(1);
-        imageGenerator.right(1);
-        imageGenerator.down(1);
-        imageGenerator.left(1);
-        imageGenerator.repeat(1);
-        imageGenerator.undo(2);
-        imageGenerator.redo(1);
+    /**
+     * Ustalenie liczby maksymalnej liczby polecen, ktore mozna cofnac, przywrocic
+     * lub powtorzyc.
+     *
+     * @param commands
+     *            maksymalna liczba polecen, ktorej moga dotyczyc operacje
+     *            undo/redo/repeat. Uwaga: w przypadku undo/redo chodzi o
+     *            <b>laczna</b> liczbe polecen, ktore sa wycofywane. Czyli, gdy
+     *            commands to 10, to moge wykonac undo(5); undo(3); undo(2).
+     */
+    public void maxUndoRedoRepeatCommands(int commands);
+}
 
-        System.out.print(imageGenerator.toString());
-    }
+interface ImageGeneratorInterface {
+
+    /**
+     * Pioro przemieszcza sie steps krokow do gory. Jesli wiersz, w ktorym
+     * poczatkowo znajduje sie pisak to row, to po wykonaniu polecenia pioro
+     * przemiesza sie do wiersza o numerze row + steps. Pozycje plotna od
+     * [col][row+1] do [col][row+N] zostaja zamalowane.
+     *
+     * @param steps
+     *            liczba krokow o jaka pioro przesunie sie w gore.
+     */
+    public void up(int steps);
+
+    /**
+     * Pioro przemieszcza sie steps krokow do dolu. Jesli wiersz, w ktorym
+     * poczatkowo znajduje sie pisak to row, to po wykonaniu polecenia pioro
+     * przemiesza sie do wiersza o numerze row - steps. Pozycje plotna od
+     * [col][row-1] do [col][row-N] zostaja zamalowane.
+     *
+     * @param steps
+     *            liczba krokow o jaka pioro przesunie sie w dol.
+     */
+    public void down(int steps);
+
+    /**
+     * Pioro przemieszcza sie steps krokow w lewo. Jesli kolumna, w ktorej
+     * poczatkowo znajduje sie pisak to col, to po wykonaniu polecenia pioro
+     * przemiesza sie do kolumny o numerze col - steps. Pozycje plotna od
+     * [col-1][row] do [col-N][row] zostaja zamalowane.
+     *
+     * @param steps
+     *            liczba krokow o jaka pioro przesunie sie w lewo.
+     */
+    public void left(int steps);
+
+    /**
+     * Pioro przemieszcza sie steps krokow w prawo. Jesli kolumna, w ktorej
+     * poczatkowo znajduje sie pisak to col, to po wykonaniu polecenia pioro
+     * przemiesza sie do kolumny o numerze col + steps. Pozycje plotna od
+     * [col+1][row] do [col+N][row] zostaja zamalowane.
+     *
+     * @param steps
+     *            liczba krokow o jaka pioro przesunie sie w prawo.
+     */
+    public void right(int steps);
+
+    /**
+     * Polecenie powtorzenia ostatnich commands polecen. Polecenie nie laczy sie z
+     * undo/redo. Czyli, sekwencja undo(1) repeat(1) oznacza powtorzenie polecenia,
+     * ktore "odslonila" operacja undo, nie zas dodatkowe wykonanie operacji undo.
+     *
+     * @param commands
+     *            liczba polecen do powtorzenia
+     */
+    public void repeat(int commands);
+
+    /**
+     * Usuniecie efektu ostatnich commands polecen. Undo nie jest traktowane jako
+     * polecene, czyli sekwencja undo(2) i undo(1) prowadzi do wycofania ostatnich 3
+     * polecen, a nie do przywrocenia 2 polecen wycofanych za pomoca pierwszego
+     * uzycia undo.
+     *
+     * @param commands
+     *            liczba polecen do wycofania
+     */
+    public void undo(int commands);
+
+    /**
+     * Przywrocenie efektu commands wycofanych polecen. Redo nie jest traktowane
+     * jako polecenie. Sekwencja redo(2) i redo(1) ma doprowadzic do odtworzenia
+     * dzialania 3 polecen usunietych przez undo.
+     *
+     * @param commands
+     *            liczba polecen, ktorych efekt nalezy przywrocic
+     */
+    public void redo(int commands);
 }
 
 enum ENameOperation {
@@ -257,5 +327,3 @@ class ImageGenerator implements ImageGeneratorConfigurationInterface, ImageGener
     }
 
 }
-
-
